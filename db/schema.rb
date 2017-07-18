533776ab7031c5b0d9cb0580a82de4e36734415a
adkadziadka <industrialdog@gmail.com>
2017-07-18 22:03:23 +0200

    Update navbar and generate Picture model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717124353) do
+ActiveRecord::Schema.define(version: 20170718151723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.integer "year"
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
